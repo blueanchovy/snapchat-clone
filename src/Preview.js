10b0ad697193fe16e29cd658d25212c2fb53c189
Manish Jha <manishjhazyx@gmail.com>
2022-02-08 21:41:48 +0530

    cancel preview button, icons on preview screen

--- src/Preview.js
+++ src/Preview.js
@@ -1,22 +1,51 @@
 import React, { useEffect } from "react";
+import "./Preview.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { selectCameraImage } from "./features/cameraSlice";
+import { selectCameraImage, resetCameraImage } from "./features/cameraSlice";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import TextFieldsOutlinedIcon from "@mui/icons-material/TextFieldsOutlined";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import NoteOutlinedIcon from "@mui/icons-material/NoteOutlined";
+import MusicNoteOutlinedIcon from "@mui/icons-material/MusicNoteOutlined";
+import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
+import CropOutlinedIcon from "@mui/icons-material/CropOutlined";
+import TimerOutlinedIcon from "@mui/icons-material/TimerOutlined";
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+    navigate("../");
+  };
 
   useEffect(() => {
     if (!cameraImage) {
-      navigate(-1);
+      navigate("../");
     }
-  }, [cameraImage]);
+  }, [navigate, cameraImage]);
 
   return (
     <div className="preview">
-      <p>Hi</p>
-      <img src={camImage} alt="" />
+      <CloseOutlinedIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsOutlinedIcon />
+        <CreateOutlinedIcon />
+        <NoteOutlinedIcon />
+        <MusicNoteOutlinedIcon />
+        <AttachFileOutlinedIcon />
+        <CropOutlinedIcon />
+        <TimerOutlinedIcon />
+      </div>
+      <img src={cameraImage} alt="" />
+      <div className="preview__footer">
+        <h2>Send Now</h2>
+        <SendOutlinedIcon fontSize="smaller" className="preview__sendIcon" />
+      </div>
     </div>
   );
 }
--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,8 +1,8 @@
-import React, { useRef, useCallback, useState } from "react";
+import React, { useRef, useCallback } from "react";
 import "./WebcamCapture.css";
 import Webcam from "react-webcam";
 import RadioButtonUncheckedOutlinedIcon from "@mui/icons-material/RadioButtonUncheckedOutlined";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setCameraImage } from "./features/cameraSlice";
 import { useNavigate } from "react-router-dom";
 
